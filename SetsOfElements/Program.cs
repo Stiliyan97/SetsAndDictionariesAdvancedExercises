55fbb6fdc95dab3aa2dd5324b3a3214ebbd8b88c
Stiliyan Avramov <stiliyan199713@gmail.com>
2022-04-19 16:32:51 +0300

    Exercise 02 is commited to gith hub from softuni Sets and Dictionaries Advanced Excersise

---
+++ SetsOfElements/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SetsOfElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nm = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = nm[0];
+            int m = nm[1];
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secondSet = new HashSet<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                firstSet.Add(number);
+            }
+            for (int i = 0; i < m; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                secondSet.Add(number);
+            }
+            foreach (var number in firstSet)
+            {
+                if (secondSet.Contains(number))
+                {
+                    Console.Write(number + " ");
+                }
+            }
+        }
+    }
+}
