6f443cae97f5f97fce057682132cd26d47136737
pcbuild <pcbuild@NEWPC>
2022-04-19 16:15:15 +0300

    Add project files.

---
+++ UniqueUsernames/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniqueUsernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> usernames = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string username = Console.ReadLine();
+                if (usernames.Contains(username) == false)
+                {
+                    usernames.Add(username);
+                }
+            }
+
+            foreach (var user in usernames)
+            {
+                Console.WriteLine(user);
+            }
+        }
+    }
+}
